029de935756157648ccc00f9d8309bef26fe5587
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-11-16 01:33:36 -0300

    Adicionando rotas de transações

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,10 +2,14 @@ import { Router, Request, Response } from "express";
 
 import { getUsers } from "./controller/AdminController";
 
-import { createUser, getProfile } from './controller/UserController';
+import { createUser, getProfile, getUser } from './controller/UserController';
 
 import { loginUser } from './controller/LoginController';
 import { authMiddleware } from "./middlewares/authMiddlewares";
+
+import { getAccounts } from './controller/AccountControllers';
+
+import { get_Transactions, accomplish_transaction } from "./controller/TransactionsController";
 
 const routes = Router();
 
@@ -19,10 +23,22 @@ routes.get('/all_users', authMiddleware, getUsers);
  */
 routes.post('/create_user', createUser);
 routes.get('/profile', authMiddleware, getProfile);
+routes.get('/user', authMiddleware, getUser);
 
 /**
  * Login
  */
 routes.post('/login', loginUser);
+
+/**
+ * Rotas para Account
+ */
+routes.get('/accounts', getAccounts);
+
+/**
+ * Rotas para Transaction
+ */
+routes.get('/transactions', get_Transactions);
+routes.post('/accomplish_transaction', authMiddleware, accomplish_transaction);
 
 export default routes;

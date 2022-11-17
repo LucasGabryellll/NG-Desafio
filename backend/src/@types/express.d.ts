6867852539daa3d3aa84fa30ab291cb13b1015a6
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-11-17 01:47:23 -0300

    @yyypes

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,9 +1,14 @@
 import { User } from '../entities/User';
+import { Account } from '../entities/Account';
 
 declare global {
   namespace Express {
     export interface Request {
       user: Partial<User>
+    }
+
+    export interface Request {
+      account: Partial<Account>
     }
   }
 }

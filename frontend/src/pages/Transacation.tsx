60ddfc417bcb5648fd37a0d38e18268413f4673c
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-11-19 08:35:27 -0300

    Page to perform a Transaction

---
+++ frontend/src/pages/Transacation.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components";
+
+export function Transaction() {
+  return (
+    <>
+      <Header />
+
+      <div>
+        <p>Página para realizar uma trasação</p>
+      </div>
+    </>
+  );
+}

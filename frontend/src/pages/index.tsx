97c9dbd50e8206040d3af16fad9cf11913ff00fb
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-11-19 08:33:43 -0300

    Index.tsx

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,6 @@
+export * from './Dashboard';
+export * from './Login'
+export * from './CreateAccount';
+export * from './Profile';
+export * from './Transacation';
+export * from './DetailsTransactions';

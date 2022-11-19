3c7a8a0ad02e7eabe137713193c38b36a5ab12fe
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-11-19 08:34:44 -0300

    User Profile Page

---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components";
+
+export function Profile() {
+  return (
+    <>
+      <Header />
+
+      <div>
+        <p>Página do Profile do usuário, Após o usuário Logar na Aplicação</p>
+      </div>
+    </>
+  );
+}

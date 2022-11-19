175040c03d93cc756c2868b3ff7e45d053cea51a
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-11-19 08:33:15 -0300

    Transaction View page

---
+++ frontend/src/pages/DetailsTransactions.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components";
+
+export function DetailsTransactions() {
+  return (
+    <>
+      <Header />
+
+      <div>
+        <p>Página para visualizar os detalhes das transações realizadas.</p>
+      </div>
+    </>
+  );
+}

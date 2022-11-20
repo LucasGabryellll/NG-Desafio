06ebb85a46d6d061379605bf502271bf9854ea0e
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-11-20 20:20:12 -0300

    Routes da aplicação

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 import { Login, CreateAccount, Profile, Transaction, DetailsTransactions } from '../pages';
 
+import { AuthContext } from '../context/auth';
+
+type Props = {
+  children: JSX.Element
+}
+
 export function AppRoutes() {
+  const Private = ({ children }: Props) => {
+    const userContext = useContext(AuthContext);
+
+    if (userContext?.loading) {
+      return <div className="loading">Carregando...</div>
+    }
+
+    if (!userContext?.token) {
+      return <Navigate to={'/'} />
+    }
+
+    return children
+  }
 
   return (
     <Router>
-        <Routes>
-          <Route index path='/' element={<Login />} />
+      <Routes>
+        <Route index path='/' element={<Login />} />
 
-          <Route path='/create_account' element={<CreateAccount />} />
+        <Route path='/create_account' element={<CreateAccount />} />
 
-          <Route path='/profile' element={<Profile />} />
+        <Route path='/profile' element={<Private>
+          <Profile /></Private>} />
 
-          <Route path='/new_transaction' element={<Transaction />} />
+        <Route path='/new_transaction' element={<Private><Transaction /></Private>} />
 
-          <Route path='/details_transactions' element={<DetailsTransactions />} />
-        </Routes>
+        <Route path='/details_transactions' element={<Private><DetailsTransactions /></Private>} />
+      </Routes>
     </Router>
   );
 }
